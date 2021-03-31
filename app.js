02dbf33d0657d7be8563dd25aed16f94c11b227d
Weirong Zhang <weirongzhang2016@gmail.com>
2021-03-31 07:20:37 -0400

    test

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 app.get('/',(req,res)=>res.send('Welcome aws node!'));
 
-//const port = process.env.port || 3000;
-const port = 3000;
+const port = process.env.port || 3000;
+//const port = 3000;
 app.listen(port,()=> {
 console.log('Node REST API Server on post 3000!');
 });
