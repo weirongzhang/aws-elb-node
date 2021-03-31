2093fc3509a8ac64be1a1aa75f7ead8be622d24a
Weirong Zhang <weirongzhang2016@gmail.com>
2021-03-31 06:10:25 -0400

    port

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>res.send('Hello World!'));
-app.listen(3000,()=> {
+
+const port = process.env.port || 3000;
+app.listen(port,()=> {
 console.log('Node REST API Server on post 3000!');
 });
